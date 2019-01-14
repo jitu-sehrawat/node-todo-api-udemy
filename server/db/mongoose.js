8a50e20d48c8c73f47fce4aeb3fa6b11d48aef26
Jitender Sehrawat <jitender.sehrawat@adglobal360.com>
2019-01-15 01:23:22 +0530

    Add POST /users route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 // mongoose.connect('mongodb://jitu-sehrawat:Google123@ds157064.mlab.com:57064/udemy-node-todos-api', {useNewUrlParser: true});
 
 module.exports = {
